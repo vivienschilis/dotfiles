ec51847d5a34a53748be0c0b5e18149faece1a71
Vivien Schilis <vivien.schilis@gmail.com>
2021-02-11 19:45:51 +0000

    avoid git prompt on /vagrant

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt
-	echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) (fish_git_prompt) " "
+	if string match -rq "^/vagrant" $PWD
+		echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) " "
+	else
+		echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) (fish_git_prompt) " "
+	end
 end
