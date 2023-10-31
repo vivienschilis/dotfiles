90bf6ff9bab5106fc89ab1d8182189cae32af8ba
Vivien Schilis <vivien.schilis@gmail.com>
2023-10-31 10:02:39 +0000

    Now that vagrant is on ec2 i just need to make the prompt different

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
-	if string match -rq "^/vagrant" $PWD
-		echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) " "
+	if test "$USER" = "vagrant"
+		echo -n -s (set_color 22BBCC) [vagrant] " " (prompt_pwd)(set_color normal) (fish_git_prompt) " "
 	else
 		echo -n -s (set_color $fish_color_cwd --bold) (prompt_pwd)(set_color normal) (fish_git_prompt) " "
 	end
